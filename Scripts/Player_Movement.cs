cb5e624c04667078448125a7864602cc2b86c81b
jishan708 <jaofficial4759@gmail.com>
2026-06-06 08:34:51 +0530

    Update Player_Movement.cs

--- Scripts/Player_Movement.cs
+++ Scripts/Player_Movement.cs
@@ -1,1 +1,14 @@
+using UnityEngine;
 
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(moveX, moveY, 0f) * speed * Time.deltaTime;
+    }
+}
