cf08c929f0e57fcf7d4d2625b23bf4037ec5ed25
jishan708 <jaofficial4759@gmail.com>
2026-06-06 12:07:00 +0530

    Update Player_Controller.cs

--- Game_Scripts/Player/Player_Controller.cs
+++ Game_Scripts/Player/Player_Controller.cs
@@ -1,1 +1,17 @@
 
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveX, moveY, 0f);
+
+        transform.position += movement * speed * Time.deltaTime;
+    }
+}
