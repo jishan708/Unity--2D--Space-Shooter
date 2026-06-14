8b2b9756687e3a5c267035fdc5b7032cc5ce6e4b
jishan708 <jaofficial4759@gmail.com>
2026-06-14 09:23:46 +0530

    Update Enemy_spawn.cs

--- Game_Scripts/Enemy/Enemy_spawn.cs
+++ Game_Scripts/Enemy/Enemy_spawn.cs
@@ -1,1 +1,31 @@
+using UnityEngine;
 
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform player;
+
+    public float minSpawnDistance = 5f;
+    public float maxSpawnDistance = 8f;
+    public float spawnInterval = 2f;
+    void Start()
+    {
+        InvokeRepeating(nameof(spawnenemy), 1f, spawnInterval);
+    }
+
+    private void spawnenemy()
+    {
+        float RandomAngle = Random.Range(0f, 360f);
+
+        // Angle to direction conversion
+        Vector2 spawnDirection = new Vector2(Mathf.Cos(RandomAngle * Mathf.Deg2Rad), Mathf.Sin(RandomAngle * Mathf.Deg2Rad));
+
+        float spawnDistance = Random.Range(minSpawnDistance, maxSpawnDistance);
+
+        Vector2 spawnPosition = (Vector2)player.position + spawnDirection * spawnDistance;
+
+        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+    }
+    
+
+}
